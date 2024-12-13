3cc6b74e208514d1444f7dc4b73d93257550d6e6
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2024-12-13 19:40:34 -0300

    adding navbar component, footer component and HomeView

---
+++ src/components/Footer.vue
@@ -0,0 +1,21 @@
+<template>
+  <footer class="bg-gray-800 text-white text-center p-4">
+    <div
+      class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
+    >
+      <a
+        href="https://www.linkedin.com/in/alexander-manriquez/"
+        class="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-gray-700 hover:text-gray-300"
+        ><i class="fa-brands fa-linkedin text-white"></i
+      ></a>
+      <a
+        href="https://github.com/AlexanderManriquez"
+        class="rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-gray-700 hover:text-gray-300"
+      >
+        <i class="fa-brands fa-github text-white"></i>
+      </a>
+
+      <p>&copy; 2024 Alexander Manríquez. Todos los derechos reservados.</p>
+    </div>
+  </footer>
+</template>
