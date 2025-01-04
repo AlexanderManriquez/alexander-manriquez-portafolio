042d1fe7ff4940dd6d1705ece045caf56e41cc07
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2025-01-04 12:34:29 -0300

    update AboutView component with personal introduction and skills section

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,49 @@
 <template>
-    Acerca de mi
+  <div class="container mx-auto p-4">
+    <h1 class="text-center text-2xl font-semibold">Acerca de Mi</h1>
+    <section class="about">
+      <p class="mt-4 px-6">
+        Después de más de 10 años en ventas y servicio al cliente, decidí reinventar mi carrera profesional siguiendo una pasión latente: el desarrollo web. Mi experiencia previa me ha enseñado la importancia de entender y satisfacer las necesidades de los usuarios, algo que considero esencial en el diseño y desarrollo de interfaces que sean funcionales y accesibles. Esta transición no fue casual; fue el resultado de una curiosidad constante por la tecnología y una profunda dedicación a aprender habilidades que transforman ideas en soluciones digitales.
+      </p>
+      <p class="mt-4 px-6">
+        Durante mi formación como desarrollador front-end, he perfeccionado habilidades en HTML, CSS, JavaScript, entre otros. Mis proyectos reflejan no solo mi capacidad técnica, sino también un enfoque orientado al usuario que adquirí durante mi experiencia previa. Me he enfocado en crear aplicaciones intuitivas, con especial atención a los detalles visuales y la experiencia de usuario, para garantizar que las soluciones que desarrollo sean tan prácticas como atractivas.
+      </p>
+      <p class="mt-4 px-6">
+        Estoy entusiasmado por la oportunidad de unir mi pasado profesional con mi presente en desarrollo web. Mis habilidades en comunicación, resolución de problemas y trabajo en equipo me permiten integrarme fácilmente en cualquier entorno laboral. Estoy listo para aportar valor a cualquier equipo, aprendiendo y creciendo junto a profesionales con los que comparta la visión de crear experiencias digitales memorables.
+      </p>
+    </section>
+
+    <section class="skills flex justify-center">
+        <ul class="p-6">
+        <li>&nbsp;</li>
+        <li class="font-semibold">Mis Habilidades</li>
+      </ul>
+      <ul class="p-6">
+        <li><i class="fa-brands fa-html5"></i> HTML 5</li>
+        <li><i class="fa-brands fa-css"></i> CSS 3</li>
+        <li><i class="fa-brands fa-js"></i> JavaScript</li>
+      </ul>
+      <ul class="p-6">
+        <li><i class="fa-brands fa-git"></i> Git</li>
+        <li><i class="fa-solid fa-wind"></i> Tailwind</li>
+        <li><i class="fa-brands fa-sass"></i> Sass</li>
+      </ul>
+      <ul class="p-6">
+        <li><i class="fa-brands fa-github"></i> Github</li>
+        <li><i class="fa-brands fa-bootstrap"></i> Bootstrap</li>
+        <li><i class="fa-brands fa-vuejs"></i>   Vue.js</li>
+      </ul>
+    </section>
+
+    <section class="contact-me">
+      <p class="text-center">
+        Si estás buscando un desarrollador Front End comprometido, creativo y apasionado, ¡no dudes
+        en contactarme!
+      </p>
+      <div class="block mx-auto mt-4 p-4 font-bold w-1/4 bg-slate-800 rounded-xl">
+        <a href="mailto:alexander.manriquez.s@gmail.com" class="block text-center"> Envíame un mensaje</a>
+      </div>     
+    </section>
+  </div>
 </template>
+
