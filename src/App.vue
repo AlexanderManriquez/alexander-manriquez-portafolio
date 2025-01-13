f12ffd7302e2c610a5db8a8c4057ac1b12a05563
AlexanderManriquez <alexander.manriquez.s@gmail.com>
2025-01-13 13:48:59 -0300

    update App component to enhance background styling with gradient animation

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,6 @@ import Footer from './components/Footer.vue'
 <template>
   <div class="app">
     <NavBar />
-
     <RouterView />
     <Footer />
   </div>
@@ -14,11 +13,18 @@ import Footer from './components/Footer.vue'
 
 <style>
 body {
-  background-color: #D9EDBF;
+  background: linear-gradient(270deg, #D9EDBF, #2c7865a1, #D9EDBF);
+  background-size: 600% 600%;
+  animation: gradientAnimation 16s ease infinite;
   color: #2C7865;
   font-family: "Baskervville", serif;
   font-weight: 500;
   font-style: normal;
 }
 
+@keyframes gradientAnimation {
+  0% { background-position: 0% 50%; }
+  50% { background-position: 100% 50%; }
+  100% { background-position: 0% 50%; }
+}
 </style>
